63305cf05b1f6041008ca5fa6ab5e6fa06f88d97
Javier Fernández <leqjl93@hotmail.com>
2023-09-27 13:28:21 -0600

    x

--- controller/controller.js
+++ controller/controller.js
@@ -10,6 +10,9 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
 });
 
+
+console.log(process.env)
+
 const openai = new OpenAIApi(configuration);
 
 const uploadfile = function(openai) {
